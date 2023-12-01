c85395e76409a913e30d85986eaf1f305ff97867
Elena <150579800+mele7na@users.noreply.github.com>
2023-12-01 22:30:14 +0300

    Add files via upload

---
+++ desserts.py
@@ -0,0 +1,38 @@
+def printMenu(): 
+    print("---------------------------")
+    print("Select an option:")
+    print("1.ice cream")
+    print("2.pie")
+    print("3.cake")
+    print("4.brownies")
+    print("5.cupcakes")
+    print("6.cheesecake")
+    print("7.pudding")
+    print("8.cookies")
+    print("9.macaroons")
+    print("10.donuts")
+    print("---------------------------")
+
+print-menu()
+option = input("Enter an option:")
+
+if option =="1":
+  print("ice cream")
+elif option =="2":
+    print("pie")
+elif option =="3":
+     print("cake")
+elif option =="4":
+     print("brownies")
+elif option =="5":
+     print("cupcakes")
+elif option =="6":
+     print("cheesecake")
+elif option =="7":
+     print("pudding")
+elif option =="8":
+     print("cookies")
+elif option =="9":
+     print("macaroons")
+elif option =="10":
+     print("donuts")
