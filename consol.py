bc9bf5f0ca0869e8a2750ce0d780782e8699945e
Elena <150579800+mele7na@users.noreply.github.com>
2023-12-02 11:36:48 +0300

    consol.py
    this prjects is about a  dessert restorant thıs ıs about ordering for it

---
+++ consol.py
@@ -0,0 +1,49 @@
+def AnaMenu():
+    #print("\033[1;32;40m")
+    #print("╔"+"═"*20+"╗")
+    print("╔═════════════════════╗")
+    print("║     DESSERTS        ║")
+    print("║                     ║")
+    print("║ 1-Cookies           ║")
+    print("║ 2-Pie               ║")
+    print("║ 3-Cheesecake        ║")
+    print("║ 4-Cupcake           ║")
+    print("║ 5-Brownies          ║")
+    print("║ 6-Ice Cream         ║")
+    print("║ 7-Pudding           ║")
+    print("║ 8-Macaroons         ║")
+    print("║ 9-Biscuits          ║") 
+    print("║ 10-Cake             ║")
+    print("║   Seçimiz nedir?    ║") 
+    print("╚═════════════════════╝")
+    secim = input()
+
+    print("Seçiniz:", secim)
+    if secim == "1":
+       print("Cookies")
+    if secim == "2":
+       print("Pie")
+    if secim =="3":
+       print("Cheesecake ")
+    if secim == "4":
+       print("Cupcake")
+    if secim == "5":
+       print("Brownies")
+    if secim == "6":
+       print("Ice Cream")
+    if secim == "7":
+       print("Pudding")
+    if secim == "8":
+       print("Macaroons")
+    if secim =="9":
+       print("Biscuits")
+    if secim =="10":
+       print("Cake")
+    print("Bizi sectiginiz icin tesekkurler")
+    # 201 ╔
+    # 205 ═
+    # 187 ╗
+    # 186 ║
+    # 200 ╚
+    # 188 ╝
+AnaMenu()
